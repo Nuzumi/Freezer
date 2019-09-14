3001a7ec96228b74b54e16a837e81cae372ce253
Nuzumi <michal.sewerniak@o2.pl>
2019-09-14 14:41:47 +0200

    work work

---
+++ Assets/Scripts/Services/JsonService.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Fridge.Services
+{
+    public interface IJsonService
+    {
+        string Serialize(object toSerialize);
+        T Deserialize<T>(string json);
+    }
+
+    public class JsonService : IJsonService
+    {
+        public T Deserialize<T>(string json)
+        {
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+
+        public string Serialize(object toSerialize)
+        {
+            return JsonConvert.SerializeObject(toSerialize);
+        }
+    }
+}
+
